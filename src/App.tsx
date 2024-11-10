6f47598cbf90aa2dc28e0e554c3d86adca1ab2b5
Bogdans Ozerkins <b.ozerkins@outlook.com>
2024-11-09 23:59:49 -0800

    add pages and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Tests from "./pages/Tests";
+import React from 'react';
 
 const App: React.FC = () => {
-    return <>
-        <link rel="stylesheet" href="themes/default.css" type="text/css" />
+    return (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} /> Home
-                <Route path="/tests" element={<Tests />} /> Tests
-            </Routes>
+            <link rel="stylesheet" href="themes/default.css" type="text/css" />
+            <div className="app">
+                {/* Header */}
+                <header className="header">
+                    <h1>Pivot Table Demo Page</h1>
+                </header>
+
+                {/* Main Content Area with Links to Demos */}
+                <main className="mainContent">
+                    <h2>Explore Pivot Table Demos</h2>
+                    <p>Select a demo to view different pivot table functionalities:</p>
+                    <ul className="demoLinks">
+                        <li><Link to="/randomized">Randomized</Link></li>
+                        <li><Link to="/tests">Tests</Link></li>
+                    </ul>
+                </main>
+
+                {/* Routes for each Demo */}
+                <Routes>
+                    <Route path="/" element={<div></div>} /> Home
+                    <Route path="/randomized" element={<Home />} /> Home
+                    <Route path="/tests" element={<Tests />} /> Tests
+                </Routes>
+
+                {/* Footer */}
+                <footer className="footer">
+                    <p>&copy; 2024 Pivot Table Demo Page</p>
+                </footer>
+            </div>
         </Router>
-    </>
-}
+
+    );
+};
 
 export default App;
