d1e9c2ada3802e25b0ca918818a183c92c49e1f1
Bogdans Ozerkins <b.ozerkins@outlook.com>
2025-04-19 15:28:15 -0700

    add pivot table test for array of arrays format

--- tests/PivotTable.test.tsx
+++ tests/PivotTable.test.tsx
@@ -27,7 +27,7 @@ describe('PivotTable', () => {
 });
 
 describe('PivotTable - data formats', () => {
-    it(`renders basic json input correctly`, () => {
+    it(`renders getting started example correctly`, () => {
         // The data that is morphed into a pivot during rendering.
         // Note that the dataset should be unique between dimensions + pivots.
         // In case when duplicate records are found, the pivot table displays the last value.
@@ -58,34 +58,34 @@ describe('PivotTable - data formats', () => {
         `.trim());
     });
 
-    // it(`renders basic json input correctly`, () => {
-    //     // The data that is morphed into a pivot during rendering.
-    //     // Note that the dataset should be unique between dimensions + pivots.
-    //     // In case when duplicate records are found, the pivot table displays the last value.
-    //     const data = [
-    //         ['Furniture', 'Chair', 'North', 100],
-    //         ['Furniture', 'Table', 'North', 250],
-    //         ['Electronics', 'Phone', 'South', 300],
-    //         ['Electronics', 'Laptop', 'South', 1200],
-    //     ];
+    it(`renders getting started example with array of arrays data correctly`, () => {
+        // The data that is morphed into a pivot during rendering.
+        // Note that the dataset should be unique between dimensions + pivots.
+        // In case when duplicate records are found, the pivot table displays the last value.
+        const data = [
+            ['Furniture', 'Chair', 'North', 100],
+            ['Furniture', 'Table', 'North', 250],
+            ['Electronics', 'Phone', 'South', 300],
+            ['Electronics', 'Laptop', 'South', 1200],
+        ];
 
-    //     // Render the data.
-    //     // Note that pivot tables do not aggregate the data, but merely display it
-    //     const table = <PivotTable
-    //         rows={data}
-    //         fields={[
-    //             { id: 'category', name: 'Category' },
-    //             { id: 'product', name: 'Product' },
-    //             { id: 'region', name: 'Region' },
-    //             { id: 'sales', name: 'Sales' }
-    //         ]}
-    //         dimensions={['category', 'product']}
-    //         measures={['sales']}
-    //         pivots={['region']}
-    //     />
-    //     const { container } = render(table);
-    //     expect(container.innerHTML).toBe(`
-    //         <div class="explore-table-container explore-smooth-scroll"><table class="explore-table"><tbody><tr class="explore-table-row"><td class="explore-pivot-title" colspan="2">Region</td><td class="explore-pivot-value" colspan="1">North</td><td class="explore-pivot-value" colspan="1">South</td></tr><tr class="explore-table-row"><td class="explore-column-title">Category</td><td class="explore-column-title">Product</td><td class="explore-measure-title">Sales</td><td class="explore-measure-title">Sales</td></tr><tr class="explore-table-row"><td class="explore-column-value">Furniture</td><td class="explore-column-value">Chair</td><td class="explore-measure-value">100</td><td class="explore-measure-value explore-empty-sign">∅</td></tr><tr class="explore-table-row"><td class="explore-column-value">Furniture</td><td class="explore-column-value">Table</td><td class="explore-measure-value">250</td><td class="explore-measure-value explore-empty-sign">∅</td></tr><tr class="explore-table-row"><td class="explore-column-value">Electronics</td><td class="explore-column-value">Phone</td><td class="explore-measure-value explore-empty-sign">∅</td><td class="explore-measure-value">300</td></tr><tr class="explore-table-row"><td class="explore-column-value">Electronics</td><td class="explore-column-value">Laptop</td><td class="explore-measure-value explore-empty-sign">∅</td><td class="explore-measure-value">1200</td></tr></tbody></table></div>
-    //     `.trim());
-    // });
+        // Render the data.
+        // Note that pivot tables do not aggregate the data, but merely display it
+        const table = <PivotTable
+            rows={data}
+            fields={[
+                { id: 'category', name: 'Category' },
+                { id: 'product', name: 'Product' },
+                { id: 'region', name: 'Region' },
+                { id: 'sales', name: 'Sales' }
+            ]}
+            dimensions={['category', 'product']}
+            measures={['sales']}
+            pivots={['region']}
+        />
+        const { container } = render(table);
+        expect(container.innerHTML).toBe(`
+            <div class="explore-table-container explore-smooth-scroll"><table class="explore-table"><tbody><tr class="explore-table-row"><td class="explore-pivot-title" colspan="2">Region</td><td class="explore-pivot-value" colspan="1">North</td><td class="explore-pivot-value" colspan="1">South</td></tr><tr class="explore-table-row"><td class="explore-column-title">Category</td><td class="explore-column-title">Product</td><td class="explore-measure-title">Sales</td><td class="explore-measure-title">Sales</td></tr><tr class="explore-table-row"><td class="explore-column-value">Furniture</td><td class="explore-column-value">Chair</td><td class="explore-measure-value">100</td><td class="explore-measure-value explore-empty-sign">∅</td></tr><tr class="explore-table-row"><td class="explore-column-value">Furniture</td><td class="explore-column-value">Table</td><td class="explore-measure-value">250</td><td class="explore-measure-value explore-empty-sign">∅</td></tr><tr class="explore-table-row"><td class="explore-column-value">Electronics</td><td class="explore-column-value">Phone</td><td class="explore-measure-value explore-empty-sign">∅</td><td class="explore-measure-value">300</td></tr><tr class="explore-table-row"><td class="explore-column-value">Electronics</td><td class="explore-column-value">Laptop</td><td class="explore-measure-value explore-empty-sign">∅</td><td class="explore-measure-value">1200</td></tr></tbody></table></div>
+        `.trim());
+    });
 })
